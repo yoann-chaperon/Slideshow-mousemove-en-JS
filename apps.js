78120d0a2556ab5cdbb80070b943965382415cb3
yoann <yochaps@gmail.com>
2022-01-02 17:43:50 +0100

    ajoute touch Mobile

--- apps.js
+++ apps.js
@@ -1,20 +1,21 @@
-const slider = document.querySelector('.slideshow-container')
+const slider = document.querySelector(".slideshow-container");
 let holding = false;
 let firstClickX;
 let alReadyLeftScrolled;
 let velocity;
 let rafID;
 
-slider.addEventListener('mousedown', e => {
+slider.addEventListener("mousedown", (e) => {
     holding = true;
 
     firstClickX = e.pageX - slider.offsetLeft;
 
     alReadyLeftScrolled = slider.scrollLeft;
-    stopTransition()
+
+    stopTransition();
 });
 
-slider.addEventListener('mousemove', e => {
+slider.addEventListener("mousemove", (e) => {
     if (!holding) return;
 
     const x = e.pageX - slider.offsetLeft;
@@ -29,122 +30,57 @@ slider.addEventListener('mousemove', e => {
     console.log(velocity);
 });
 
-slider.addEventListener('mouseup', () => {
-    holding = false
-    startTransition()
+slider.addEventListener("mouseup", () => {
+    holding = false;
+    startTransition();
 });
 
-slider.addEventListener('mouseleave', () => {
-    holding = false
-})
+slider.addEventListener("mouseleave", () => {
+    holding = false;
+});
 
-function startTransition(){
-
+function startTransition() {
     stopTransition();
 
     rafID = requestAnimationFrame(decreasingTransition);
 }
 
-function stopTransition(){
-    cancelAminationFrame(rafID)
+function stopTransition() {
+    cancelAnimationFrame(rafID);
 }
 
 function decreasingTransition() {
     slider.scrollLeft += velocity;
     velocity *= 0.95;
-    if(Math.abs(velocity) > 0.5){
-        rafID =requestAnimationFrame(decreasingTransition)
+    if (Math.abs(velocity) > 0.5) {
+        rafID = requestAnimationFrame(decreasingTransition);
+        console.log(velocity);
     }
 }
+// ------------ MOBILE ---------------//
 
+slider.addEventListener("touchstart", (e) => {
+    holding = true;
+    // pageX => la largeur entre mon click et le DOCUMENT
+    firstClickX = e.targetTouches[0].pageX - slider.offsetLeft;
 
+    alreadyLeftScrolled = slider.scrollLeft;
+    stopTransition();
+});
+slider.addEventListener("touchend", () => {
+    holding = false;
+    startTransition();
+});
+slider.addEventListener("touchmove", (e) => {
+    if (!holding) return;
 
-// -------------------------------
+    const x = e.targetTouches[0].pageX - slider.offsetLeft;
 
-// const slider = document.querySelector('.slideshow-container')
-// let holding = false;
-// let firstClickX;
-// let alreadyLeftScrolled;
-// let velocity;
-// let rafID;
+    const scrolled = (x - firstClickX) * 2;
 
-// slider.addEventListener('mousedown', e => {
-//     holding = true;
+    const prevScrollLeft = slider.scrollLeft;
 
-//     firstClickX = e.pageX - slider.offsetLeft;
+    slider.scrollLeft = alreadyLeftScrolled - scrolled;
 
-//     alreadyLeftScrolled = slider.scrollLeft;
-
-//     stopTransition()
-// })
-
-// slider.addEventListener('mousemove', e => {
-//     if (!holding) return;
-
-//     const x = e.pageX - slider.offsetLeft;
-
-//     const scrolled = (x - firstClickX) * 2;
-
-//     const prevScrollLeft = slider.scrollLeft
-
-//     slider.scrollLeft = alreadyLeftScrolled - scrolled;
-
-//     velocity = slider.scrollLeft - prevScrollLeft;
-
-// })
-
-// // slider.addEventListener('mouseup', () => {
-// //     holding = false;
-// //     startTransition()
-// // })
-// // slider.addEventListener('mouseleave', () => {
-// //     holding = false;
-// // })
-
-// // function startTransition() {
-
-// //     stopTransition();
-
-// //     rafID = requestAnimationFrame(decreasingTransition);
-// // }
-
-// // function stopTransition() {
-// //     cancelAnimationFrame(rafID)
-// // }
-
-// // function decreasingTransition() {
-
-// //     slider.scrollLeft += velocity;
-// //     velocity *= 0.95;
-// //     if (Math.abs(velocity) > 0.5) {
-// //         rafID = requestAnimationFrame(decreasingTransition)
-// //         console.log(velocity);
-// //     }
-
-// // }
-
-// // slider.addEventListener('touchstart', e => {
-// //     holding = true;
-// //     // pageX => la largeur entre mon click et le DOCUMENT
-// //     firstClickX = e.targetTouches[0].pageX - slider.offsetLeft;
-
-// //     alreadyLeftScrolled = slider.scrollLeft;
-// //     stopTransition()
-// // })
-// // slider.addEventListener('touchend', () => {
-// //     holder = false;
-// //     startTransition()
-// // })
-// // slider.addEventListener('touchmove', e => {
-// //     if (!holding) return;
-
-// //     const x = e.targetTouches[0].pageX - slider.offsetLeft;
-
-// //     const scrolled = (x - firstClickX) * 2;
-
-// //     const prevScrollLeft = slider.scrollLeft;
-
-// //     slider.scrollLeft = alreadyLeftScrolled - scrolled;
-
-// //     velocity = slider.scrollLeft - prevScrollLeft;
-// // });
+    velocity = slider.scrollLeft - prevScrollLeft;
+});
